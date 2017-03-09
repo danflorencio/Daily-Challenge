3ffe6ef00df99487f096b71de10e3f7066c289b0
Daniel Florencio <danflorencio94@gmail.com>
2017-03-09 15:43:36 -0800

    Minimum viable product
    It works! Now I just have to put it on the chrome web store, and improve upon it in future iterations, but I got a midterm an hour.

--- DailyChallenge/popup.js
+++ DailyChallenge/popup.js
@@ -5,14 +5,29 @@
  * grab JSON data from Codewars.
  */
 
+// jQuery for API requests
 var theNewScript = document.createElement("script");
 theNewScript.type = "text/javascript";
 theNewScript.src = "jquery.js";
+
+// To keep track of what question a user is on
+var qNum;
+
 var p = "";
+
+var bgPage;
 
 window.onload = function() {
   // Debug
   console.log("window.onload");
+  // Go to previous question
+  document.getElementById("prev").addEventListener("click", decrement);
+  document.getElementById("next").addEventListener("click", increment);
+  bgPage = chrome.extension.getBackgroundPage();
+  /*chrome.storage.local.get(qNo, function(result) {
+    qNo = result.qNo;
+  });*/
+  qNum = bgPage.getQNo();
 }
 
 window.addEventListener ("load", readfileautomatically, false);
@@ -39,10 +54,31 @@ function readfileautomatically () {
     if(client.responseText != '')
     {
       var txt = client.responseText.split("\n");
-      $.get(txt[0], function(data) {
+      $.get(txt[qNum], function(data) {
+        document.getElementById("questionName").innerHTML = "Today's Question: " + data.name;
         document.getElementById("question").innerHTML = data.description;
+        document.getElementById("questionLink").innerHTML = data.url;
+        document.getElementById("questionLink").href = data.url;
       })
     }
   }
   client.send();
+}
+
+// Make the question link in popup.html clickable so that a user can open it in
+// a new tab
+window.addEventListener('click', function(e){
+  if(e.target.href !== undefined){
+    chrome.tabs.create({url:e.target.href})
+  }
+})
+
+function decrement() {
+  qNum = bgPage.decQNo();
+  readfileautomatically();
+}
+
+function increment() {
+  qNum = bgPage.incQNo();
+  readfileautomatically();
 }
